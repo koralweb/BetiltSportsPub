b081daf418f33451398ab9f6aa5888a224b5ed22
Svetlana1989 <sveta-marki@mail.ru>
2023-05-29 14:10:55 +0500

    final

--- src/screens/BookingScreen.js
+++ src/screens/BookingScreen.js
@@ -9,24 +9,38 @@ const BookingScreen = ({navigation}) => {
       <Header navigation={navigation} />
 
       <Text style={style.title}>Бронирование</Text>
-      <TextInput
-          style={style.input}
-          placeholder="Имя"
-        />
-        <TextInput
-          style={style.input}
-          placeholder="+000 000"
-        />
-        <TextInput
-          style={style.input}
-          placeholder="Электронная почта"
-        />
-        <TextInput
-          style={style.input}
-          placeholder="Дата и время"
-        />
+
+      <View style={style.input}>
+        <TextInput placeholder="Имя"/>
+         <TouchableOpacity style={style.btn}>
+          <Text style={style.btntext}>Имя</Text>
+         </TouchableOpacity>
+      </View>
+
+      <View style={style.input}>
+        <TextInput placeholder="+000 000"/>
+         <TouchableOpacity style={style.btn}>
+          <Text style={style.btntext}>Номер телефона</Text>
+         </TouchableOpacity>
+      </View>
+
+      <View style={style.input}>
+        <TextInput placeholder="Электронная почта"/>
+         <TouchableOpacity style={style.btn}>
+          <Text style={style.btntext}>Почта</Text>
+         </TouchableOpacity>
+      </View>
+
+      <View style={style.input}>
+        <TextInput placeholder="Дата и время"/>
+         <TouchableOpacity style={style.btn}>
+          <Text style={style.btntext}>Дата и время</Text>
+         </TouchableOpacity>
+      </View>
+      
+
          <TouchableOpacity
-          style={style.btn}
+          style={style.checkout}
           onPress={() => navigation.push('Checkout')}>
           <Text style={style.button}>Забронировать</Text>
         </TouchableOpacity>
@@ -49,6 +63,8 @@ const style= StyleSheet.create({
     margin: 20,
   },
   input: {
+    flexDirection: 'row',
+    justifyContent: "space-between",
     borderWidth: 1,
     width: 350,
     height: 40,
@@ -59,34 +75,44 @@ const style= StyleSheet.create({
     fontSize: 15,
     paddingLeft: 15,
     marginTop: 5,
-
   },
   logo: {
     width: 423 / 4, 
     height: 153 / 4, 
     alignSelf: "center", 
     marginTop: 150,
-
   },
-  btn: {
-    padding: 5,
+  checkout: {
+    paddingTop: 5,
     borderWidth: 1,
-    width: 200,
+    width: 300,
     marginTop: 100,
     marginLeft: 'auto',
     marginRight: 'auto',
-    width: 350,
     height: 40,
     backgroundColor: '#006600',
     color: 'white',
-
   },
   button: {
     color: 'white',
     fontSize: 20,
     fontWeight: 700,
     alignSelf: 'center',
-    
+  },
+  btn: {
+   marginRight: 5,
+   marginTop: 5,
+  },
+  btntext: {
+    borderWidth: 1,
+    borderColor: 'grey',
+    borderRadius: 4,
+    width: 100, 
+    height: 25, 
+    alignSelf: 'center',
+    marginTop: 2,
+    color: "black",
+    textAlign: 'center',
   },
 
 
